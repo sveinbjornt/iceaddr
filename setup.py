1344d741959b2b306620b0c37ffb6d9257f0426b
Sveinbjorn Thordarson <sveinbjornt@gmail.com>
2018-10-10 14:52:32 +0000

    sqlite3 is part of Python's standard library, silly me

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="iceaddr",
-    version="0.1.0",
+    version="0.1.2",
     author="Sveinbjorn Thordarson",
     author_email="sveinbjorn@sveinbjorn.org",
     license='BSD',
@@ -15,9 +15,7 @@ setuptools.setup(
     description="Look up information about Icelandic street addresses and postcodes",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=[
-        'sqlite3'
-    ],
+    install_requires=[],
     packages=['iceaddr'],
     classifiers=[
         "License :: OSI Approved :: BSD License",
@@ -26,8 +24,13 @@ setuptools.setup(
         "Intended Audience :: Developers",
         "Natural Language :: Icelandic",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Utilities",
         "Topic :: Text Processing :: Linguistic"
