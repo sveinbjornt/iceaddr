12205bcd3bb4df8facbfec42a056faaccfe09c4a
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2020-10-16 16:31:40 +0000

    Fixed bug in placename data extraction utility, rm unused code

--- add_placename_data.py
+++ add_placename_data.py
@@ -6,7 +6,6 @@
 """
 
 import fiona
-import math
 import sqlite3
 from pprint import pprint
 
@@ -20,58 +19,6 @@ LAYERS = [
 ]
 
 DEFAULT_DBNAME = "iceaddr.db"
-
-
-def isnet93_to_wgs84(xx, yy):
-    """ Convert isnet93 coordinates to WGS84.
-        From https://github.com/pallih/Scraperwiki-scrapers/blob/master/isnet93_wgs84.py
-    """
-    x = xx
-    y = yy
-    a = 6378137.0
-    f = 1 / 298.257222101
-    lat1 = 64.25
-    lat2 = 65.75
-    latc = 65.00
-    lonc = 19.00
-    eps = 0.00000000001
-
-    def fx(p):
-        return a * math.cos(p / rho) / math.sqrt(1 - math.pow(e * math.sin(p / rho), 2))
-
-    def f1(p):
-        return math.log((1 - p) / (1 + p))
-
-    def f2(p):
-        return f1(p) - e * f1(e * p)
-
-    def f3(p):
-        return pol1 * math.exp((f2(math.sin(p / rho)) - f2sin1) * sint / 2)
-
-    rho = 45 / math.atan2(1.0, 1.0)
-    e = math.sqrt(f * (2 - f))
-    dum = f2(math.sin(lat1 / rho)) - f2(math.sin(lat2 / rho))
-    sint = 2 * (math.log(fx(lat1)) - math.log(fx(lat2))) / dum
-    f2sin1 = f2(math.sin(lat1 / rho))
-    pol1 = fx(lat1) / sint
-    polc = f3(latc) + 500000.0
-    peq = (
-        a
-        * math.cos(latc / rho)
-        / (sint * math.exp(sint * math.log((45 - latc / 2) / rho)))
-    )
-    pol = math.sqrt(math.pow(x - 500000, 2) + math.pow(polc - y, 2))
-    lat = 90 - 2 * rho * math.atan(math.exp(math.log(pol / peq) / sint))
-    lon = 0
-    fact = rho * math.cos(lat / rho) / sint / pol
-    fact = rho * math.cos(lat / rho) / sint / pol
-    delta = 1.0
-    while math.fabs(delta) > eps:
-        delta = (f3(lat) - pol) * fact
-        lat += delta
-    lon = -(lonc + rho * math.atan((500000 - x) / (polc - y)) / sint)
-
-    return {"lat": round(lat, 7), "lng": round(lon, 7)}
 
 
 def center_point(coords):
@@ -106,13 +53,14 @@ def create_table(dbpath):
     try:
         dbconn.cursor().execute(create_table_sql)
     except Exception:
-        print("Unable to create ornefni table")
+        print("Unable to create table 'ornefni'")
         exit()
 
     return dbconn
 
 
 def delete_table(dbpath):
+    """ Drop ornefni database table. """
     dbconn = sqlite3.connect(dbpath)
 
     del_table_sql = """DROP TABLE ornefni"""
@@ -138,7 +86,7 @@ if not dbc:
 cursor = dbc.cursor()
 
 
-# Read manual placename additions from text file, insert into DB
+# Read manual placename additions from text file, insert into ornefn DB table
 print("Inserting placename additions")
 f = open("placename_additions.txt", "r")
 for line in f.readlines():
@@ -155,29 +103,11 @@ for line in f.readlines():
         print(line)
         raise
 
-    # If so, update it
-    # Else, insert it
-
-    # Check if already exists in name and category and is single
-    res = cursor.execute(
-        "SELECT * FROM ornefni WHERE nafn=? AND flokkur=?", [first, fl]
+    print("Inserting " + first)
+    cursor.execute(
+        "INSERT INTO ornefni (nafn, flokkur, lat_wgs84, long_wgs84) VALUES (?,?,?,?)",
+        (first, fl, lat, lon),
     )
-    # print(res)
-    matches = [row for row in res]
-    if len(matches) > 1:
-        print("More than one match for " + first)
-    elif len(matches) == 1:
-        print("Updating " + first)
-        theid = matches[0][0]
-        q = "UPDATE ornefni SET nafn=?, flokkur=?, lat_wgs84=?, long_wgs84=? WHERE id=?"
-        qargs = [first, fl, lat, lon, theid]
-        cursor.execute(q, qargs)
-    else:
-        print("Inserting " + first)
-        cursor.execute(
-            "INSERT INTO ornefni (nafn, flokkur, lat_wgs84, long_wgs84) VALUES (?,?,?,?)",
-            (first, fl, lat, lon),
-        )
 
 dbc.commit()
 
@@ -218,7 +148,6 @@ for layer in fiona.listlayers(GPKG_FILE):
             # Special handling of flakes - use center point
             elif type(c) is list:
 
-                # pprint(c)
                 if not c:
                     # print("Faulty flake: {0}".format(n))
                     # pprint(i)
@@ -238,7 +167,8 @@ for layer in fiona.listlayers(GPKG_FILE):
             else:
                 cp = c
 
-            gps = (cp[0], cp[1])
+            # LMÍ's GPKG coord values are reversed! Why?
+            gps = (cp[1], cp[0])
 
             # Insert
             cursor.execute(
