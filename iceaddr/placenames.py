5fb6eb05ac2088624a66796e077d1c8d6d3aad4f
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2021-06-29 15:26:03 +0000

    Updated copyright header

--- iceaddr/placenames.py
+++ iceaddr/placenames.py
@@ -3,7 +3,7 @@
     iceaddr: Look up information about Icelandic streets, addresses,
              placenames, landmarks, locations and postcodes.
 
-    Copyright (c) 2018-2020 Sveinbjorn Thordarson.
+    Copyright (c) 2018-2021 Sveinbjorn Thordarson.
 
     This file contains code related to placename lookup.
 
