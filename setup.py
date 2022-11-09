072b8689971e2c530092edf0f70d6f16f3ce261d
Sveinbjorn Thordarson <sveinbjorn@sveinbjorn.org>
2022-11-09 23:02:55 +0000

    Added explicit Python 3.10 and 3.11 support in setup.py

--- setup.py
+++ setup.py
@@ -34,6 +34,8 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Utilities",
         "Topic :: Text Processing :: Linguistic",
